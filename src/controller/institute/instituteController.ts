f7e206bfcccfbd6ddb35f0c07651a1ea7f22f2e0
ArjunMagar <arjunmagar6820@gmail.com>
2025-06-20 21:26:52 +0545

    create teacher,student,courseTable,applied transaction command in instituteCreate Api

--- src/controller/institute/instituteController.ts
+++ src/controller/institute/instituteController.ts
@@ -1,11 +1,14 @@
-import { Request, Response } from "express";
+import { Request, response, Response } from "express";
 import sequelize from "../../database/connection";
 import generateRandomInstituteNumber from "../../services/generateRandomInstituteNumber";
 import { AuthRequest } from "../../middleware/isAuthenticated";
 import User from "../../database/models/userModel";
+import { QueryTypes } from "sequelize";
 
 class InstituteController {
   static async createInstitute(req: AuthRequest, res: Response): Promise<void> {
+
+    // check validation 
     const userId = req.user?.id;
     if (!req.body) {
       res.status(409).json({
@@ -13,7 +16,7 @@ class InstituteController {
       });
       return;
     }
-
+    // check validation
     const {
       instituteName,
       instituteEmail,
@@ -22,6 +25,7 @@ class InstituteController {
     } = req.body;
     const instituteVatNo = req.body.instituteVatNo || null;
     const institutePanNo = req.body.institutePanNo || null;
+    // check validation 
     if (
       !instituteName ||
       !instituteEmail ||
@@ -34,6 +38,7 @@ class InstituteController {
       });
       return;
     }
+    // check validation
     if (!instituteVatNo && !institutePanNo) {
       res.status(400).json({
         message: "Please provider instituteVatNo or institutePanNo",
@@ -42,7 +47,7 @@ class InstituteController {
     }
     const instituteNumber = generateRandomInstituteNumber();
 
-    // To create userInstitutes table
+    // To create userInstitutes History table
     await sequelize.query(`CREATE TABLE IF NOT EXISTS userInstitutes(
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
         userId CHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
@@ -50,20 +55,23 @@ class InstituteController {
         FOREIGN KEY (userId) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
     )`)
 
-    // To check for creating institute by one user
-    const [data] = await sequelize.query(`SELECT * FROM userInstitutes WHERE userId = ?`, {
-      replacements: [userId]
+    // To check allow that how many times for creating institute by one user
+    const data = await sequelize.query(`SELECT * FROM userInstitutes WHERE userId = ?`, {
+      replacements: [userId],
+      type : QueryTypes.SELECT
     })
-    
-    if (data.length >= 3) {
+    if (data.length >= 1) {
       res.status(404).json({
-        message: "Limit user to a maximum of 3 associated institutes"
+        message: "You have already created a institute with that email id"
       })
       return;
     }
-    //To create institute query
-    await sequelize.query(`CREATE TABLE IF NOT EXISTS institute_${instituteNumber} (
-            id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    //Transaction applied
+    const transaction = await sequelize.transaction()
+    try {
+      //To create institute query
+      await sequelize.query(`CREATE TABLE IF NOT EXISTS institute_${instituteNumber} (
+            id VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
             instituteName VARCHAR(255) NOT NULL,
             instituteEmail VARCHAR(255) NOT NULL UNIQUE,
             institutePhoneNumber VARCHAR(255) NOT NULL UNIQUE,
@@ -74,43 +82,86 @@ class InstituteController {
             updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )`);
 
-    await sequelize.query(
-      `INSERT INTO institute_${instituteNumber} (instituteName,
+      // To insert datas in instituteTable
+      await sequelize.query(
+        `INSERT INTO institute_${instituteNumber} (instituteName,
       instituteEmail,institutePhoneNumber,instituteAddress,instituteVatNo,institutePanNo) VALUES (?,?,?,?,?,?)`,
-      {
-        replacements: [
-          instituteName,
-          instituteEmail,
-          institutePhoneNumber,
-          instituteAddress,
-          instituteVatNo,
-          institutePanNo,
-        ],
-      }
-    );
+        {
+          replacements: [
+            instituteName,
+            instituteEmail,
+            institutePhoneNumber,
+            instituteAddress,
+            instituteVatNo,
+            institutePanNo,
+          ],
+          transaction: transaction
+        }
+      );
 
-    // Insert data of userInstitute History tracking 
-    await sequelize.query(`INSERT INTO userInstitutes(userId,instituteNumber)VALUES(?,?)`, {
-      replacements: [userId, instituteNumber]
-    })
-    // current instituteNumber tracking
-    if (req.user) {
-      const user = await User.findByPk(req.user.id)
-      if (user) {
-        await User.update({
-          currentInstituteNumber: instituteNumber
-        }, {
-          where: {
-            id: req.user.id
-          }
-        })
+      //To create teacherTable
+      await sequelize.query(`CREATE TABLE IF NOT EXISTS teacher_${instituteNumber} (
+      id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      teacherName VARCHAR(255) NOT NULL,
+      teacherEmail VARCHAR(255) NOT NULL UNIQUE,
+      teacherPhoneNumber VARCHAR(255) NOT NULL UNIQUE
+      )`)
+
+      //To create studentTable
+      await sequelize.query(`CREATE TABLE IF NOT EXISTS student_${instituteNumber} (
+      id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      studentName VARCHAR(255) NOT NULL,
+      studentEmail VARCHAR(255) NOT NULL,
+      studentPhoneNumber VARCHAR(255) NOT NULL
+      )`)
+
+      //To create courseTable
+      await sequelize.query(`CREATE TABLE IF NOT EXISTS course_${instituteNumber} (
+      id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      courseName VARCHAR(255) NOT NULL,
+      coursePrice VARCHAR(255) NOT NULL
+      )`)
+
+      // Insert data in userInstitute History tracking 
+      await sequelize.query(`INSERT INTO userInstitutes(userId,instituteNumber)VALUES(?,?)`, {
+        replacements: [userId, instituteNumber],
+        transaction: transaction
+      })
+
+      // Current instituteNumber tracking
+      if (req.user) {
+        const user = await User.findByPk(req.user.id)
+        if (user) {
+          await User.update({
+            currentInstituteNumber: instituteNumber,
+
+          }, {
+            where: {
+              id: req.user.id
+            },
+            transaction: transaction
+          })
+        }
       }
 
+      await transaction.commit();
+
+      console.log("unmanaged transcation has been done")
+      res.status(200).json({
+        message: "Institute created!",
+      });
+
+    } catch (error) {
+      console.log("unmanaged transcation has been rolledback due to error", error)
+      await transaction.rollback();
+      await sequelize.query(`DROP TABLE IF EXISTS institute_${instituteNumber};`)
+      await sequelize.query(`DROP TABLE IF EXISTS teacher_${instituteNumber};`)
+      await sequelize.query(`DROP TABLE IF EXISTS student_${instituteNumber};`)
+      await sequelize.query(`DROP TABLE IF EXISTS course_${instituteNumber};`)
+      res.status(500).json({
+        message: "Institute create failed...!, Try again...!"
+      })
     }
-
-    res.status(200).json({
-      message: "Institute created!",
-    });
 
   }
 }
--- src/routes/institute/instituteRoute.ts
+++ src/routes/institute/instituteRoute.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express'
 import InstituteController from '../../controller/institute/instituteController'
 import isAuthenticated, { Role } from '../../middleware/isAuthenticated'
+import asyncErrorHandler from '../../services/asyncErrorHandler'
 
 const router:Router = express.Router()
 
-router.route("/").post(isAuthenticated.isAuthenticated,isAuthenticated.restrictTo(Role.Institute),InstituteController.createInstitute)
+router.route("/").post(isAuthenticated.isAuthenticated,isAuthenticated.restrictTo(Role.Institute),asyncErrorHandler(InstituteController.createInstitute))
 
 export default router
